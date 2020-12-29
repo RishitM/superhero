91a506aaa8917d797be0608219c234e2d69a79b1
RishitM <72683716+RishitM@users.noreply.github.com>
2020-12-29 15:38:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,35 +16,57 @@ function player_update() {
         canvas.add(player_object);
     });
 }
+function block_image_update(getImage){
+    fabric.Image.fromURL(getImage,function(img){
+        block_image_object=img;
+        block_image_object.scaleToWidth(block_image_width);
+        block_image_object.scaleToHeight(block_image_heigth);
+        block_image_object.set({
+            top:player_y,
+            left:player_x
+        });
+        canvas.add(block_image_object);
+        });
+}
 window.addEventListener("keydown", mykeydown);
 function mykeydown(e){
     keyPressed=e.keyCode;
     console.log(keyPressed);
     if (keyPressed == "70")
     {
-new_Image("ironman_face.png");
+block_image_update("ironman_face.png");
 console.log("f");
     }
     if (keyPressed == "66")
     {
-new_Image("spiderman_body.png");
+block_image_update("spiderman_body.png");
 console.log("b");
     }
     if (keyPressed == "76")
     {
-new_Image("hulk_legs.png");
+block_image_update("hulk_legs.png");
 console.log("l");
     }
     if (keyPressed == "82")
     {
-new_Image("thor_right_hand.png");
+block_image_update("thor_right_hand.png");
 console.log("r");
     }
     if (keyPressed == "72")
     {
-new_Image("captain_america_left_hand.png");
+block_image_update("captain_america_left_hand.png");
 console.log("h");
     }
+    if (keyPressed == "77"&& e.shiftKey==true) {
+        block_image_width-=10 ; block_image_heigth-=10;
+        document.getElementById("width").innerHTML= block_image_width;
+        document.getElementById("height").innerHTML= block_image_heigth;
+   }
+   if (keyPressed == "80"&& e.shiftKey==true) {
+       block_image_width+=10 ; block_image_heigth+=10;
+       document.getElementById("width").innerHTML= block_image_width;
+   document.getElementById("height").innerHTML= block_image_heigth;
+   }
     if (keyPressed == "38")
     {
 up();
@@ -65,4 +87,26 @@ console.log("left")
 right();
 console.log("right")
     }
+
+}
+function up(){
+player_y=player_y-10;
+canvas.remove(player_object);
+player_update();
+}
+function down(){
+ player_y=player_y+10;
+ canvas.remove(player_object);
+ player_update();
+ 
+}
+function left(){
+player_x=player_x-10;
+canvas.remove(player_object);
+player_update();
+}
+function right(){
+ player_x=player_x+10;
+ canvas.remove(player_object);
+ player_update();
 }
